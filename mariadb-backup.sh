86e73981b9f23adad3382620bc96208499a445e4
Neil Spink <neilspink@gmail.com>
2018-04-16 19:36:25 +0200

    database dump script fixing and completion

--- mariadb-backup.sh
+++ mariadb-backup.sh
@@ -5,32 +5,26 @@
 # The --lock-tables flag is used and will locks all tables for the duration of the mysqldump, it a bad option 
 # to use on a live environment, but, in the early hours of the morning it should not be an isse. 
 
-# You need to give in the correct OUTPUT folder below. Create the dbs folder if it doesn't exist.
-
 # Run following command after creating the file
 # chmod + mariadb-backup.sh
 
-# Then, use the following command to securely setup auto login for the script (you will be prompted for the password):
-# mysql_config_editor set --login-path=/home/ubuntu --host=localhost --user=root --password
-
-# Check the mysql login is configured:
-# mysql_config_editor print --all
-
-# Setup a cron job using the standard user, e.g. crontab -e
-# 32 0 * * * /home/ubuntu/mysql-backup.sh
+# Setup a cron job using the root user, e.g. sudo crontab -e
+# 32 0 * * * /home/ubuntu/mariadb-backup.sh
 
 # To uncompress the SQL dumps use the command line tool, e.g. gunzip <filename.sql.gz>
+
+# Note MariaDB doesn't support mysql_config_editor like MySQL :D
 
 OUTPUT="/home/ubuntu/dbs"
 
 rm $OUTPUT/*gz
 
-databases=`mysql --login-path=$OUTPUT  -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+databases=`mysql -u root -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 
 for db in $databases; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
         echo "Dumping database: $db"
-        mysqldump --login-path=$OUTPUT --lock-tables --databases $db >  $OUTPUT/`date +%Y%m%d`.$db.sql
+        mysqldump -u root --lock-tables --databases $db >  $OUTPUT/`date +%Y%m%d`.$db.sql
         gzip $OUTPUT/`date +%Y%m%d`.$db.sql
     fi
 done
